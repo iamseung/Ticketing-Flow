41c88943bcde441f26f41a28941079905bb61a2d
iamseung <seesungseok@naver.com>
2024-04-15 00:50:59 +0900

    진입 상태 조회 API 개발
    - 진입 상태 조회 API 개발 및 초안 작성

--- src/main/java/com/example/flow/service/UserQueueService.java
+++ src/main/java/com/example/flow/service/UserQueueService.java
@@ -12,8 +12,9 @@ import java.time.Instant;
 @RequiredArgsConstructor
 public class UserQueueService {
     private final ReactiveRedisTemplate<String, String> reactiveRedisTemplate;
-    
+
     private final String USER_QUEUE_WAIT_KEY = "users:queue:%s:wait";
+    private final String USER_QUEUE_PROCEED_KEY = "users:queue:%s:proceed";
 
     // 대기열 등록 API, 대기열을 다수로 운영하기 위한 설계
     public Mono<Long> registerWaitQueue(final String queue, final Long userId) {
@@ -23,5 +24,17 @@ public class UserQueueService {
                 .switchIfEmpty(Mono.error(ErrorCode.QUEUE_ALREADY_REGISTERED_USER.build()))
                 .flatMap(i -> reactiveRedisTemplate.opsForZSet().rank(USER_QUEUE_WAIT_KEY.formatted(queue), userId.toString()))
                 .map(i -> i >= 0 ? i+1: i);
+    }
+
+    // 진입이 가능한 상태인지 조회, 진입 허용
+    public Mono<?> allowUser(final String queue, final Long count) {
+        /*
+        진입을 허용하는 단계
+        1. wait queue, 사용자 제거
+        2. wait proceed, 사용자를 추가
+         */
+        return reactiveRedisTemplate.opsForZSet().popMin(USER_QUEUE_WAIT_KEY.formatted(queue), count)
+                .flatMap(member -> reactiveRedisTemplate.opsForZSet().add(USER_QUEUE_PROCEED_KEY.formatted(queue), member.getValue(), Instant.now().getEpochSecond()))
+                .count();
     }
 }
