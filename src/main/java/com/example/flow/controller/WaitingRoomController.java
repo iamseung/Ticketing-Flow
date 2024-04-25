aa5f910d9643dca1debae08211e82562ecbe7892
iamseung <seesungseok@naver.com>
2024-04-25 22:53:45 +0900

    접속 대기 api 리팩토링
    - 상황에 맞게 반환 및 redirect 세부 기능 구현

--- src/main/java/com/example/flow/controller/WaitingRoomController.java
+++ src/main/java/com/example/flow/controller/WaitingRoomController.java
@@ -1,5 +1,7 @@
 package com.example.flow.controller;
 
+import com.example.flow.service.UserQueueService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,11 +9,29 @@ import org.springframework.web.reactive.result.view.Rendering;
 import reactor.core.publisher.Mono;
 
 @Controller
+@RequiredArgsConstructor
 public class WaitingRoomController {
+    private final UserQueueService userQueueService;
+
     @GetMapping("/waiting-room")
-    Mono<Rendering> watingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
-                                   @RequestParam(name = "user_id") Long userId) {
-        return Mono.just(Rendering.view("waiting-room.html")
-                .build());
+    Mono<Rendering> waitingRoomPage(@RequestParam(name = "queue", defaultValue = "default") String queue,
+                                   @RequestParam(name = "user_id") Long userId,
+                                    @RequestParam(name = "redirect_url") String redirectUrl) {
+        /*
+        1. 입장이 바로 허용되면 redirect
+        2. 이미 등록돼서 에러가 발생한 경우, 랭크 반환
+         */
+        return userQueueService.isAllowed(queue, userId)
+                .filter(allowed -> allowed)
+                .flatMap(allowed -> Mono.just(Rendering.redirectTo(redirectUrl).build()))
+                .switchIfEmpty(
+                        userQueueService.registerWaitQueue(queue, userId)
+                                .onErrorResume(ex -> userQueueService.getRank(queue, userId))
+                                .map(rank -> Rendering.view("waiting-room.html")
+                                        .modelAttribute("number", rank)
+                                        .modelAttribute("userId", userId)
+                                        .build()
+                                )
+                );
     }
 }
